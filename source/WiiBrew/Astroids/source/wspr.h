a3a482268774c6581a42602ff156ff7cc69a51be
tomyumsky <tomyumsky@888d0ec0-acf8-11dd-9a07-0f3041a8e97c>
2008-11-16 20:55:05 +0000

    uploaded by accident

--- source/WiiBrew/Astroids/source/wspr.h
+++
@@ -1,9 +0,0 @@
-#ifndef WSPR_H
-#define WSPR_H
-
-#include "wspr_types.h"
-#include "wspr_surface.h"
-#include "wspr_graphics.h"
-#include "wspr_transform.h"
-
-#endif
--- source/WiiBrew/MyPong/build/bar_bounce_mod.h
+++
@@ -1,3 +0,0 @@
-extern const u8 bar_bounce_mod_end[];
-extern const u8 bar_bounce_mod[];
-extern const u32 bar_bounce_mod_size;
